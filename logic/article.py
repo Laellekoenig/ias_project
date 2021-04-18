f3aba5a512c29801926ddb5af6afca73226fc6d0
Simon Laube <simon.laube96@gmail.com>
2021-04-18 14:24:29 +0200

    article can return html

--- logic/article.py
+++ logic/article.py
@@ -5,6 +5,15 @@ class NewsSource(str, Enum):
     SRF = 'SRF'
     DIE_ZEIT = 'Die Zeit'
 
+class HTMLTag(str, Enum):
+    TITLE_0 = 'h3'
+    TITLE_1 = 'h1'
+    SUBTITLE = 'h2'
+    AUTHOR = 'h3'
+    DATE_AND_TIME = 'h4'
+    TAGLINE = 'h2'
+    PARAGRAPH = 'p'
+
 class Article:
     def __init__(self, news_source):
         self.news_source = news_source
@@ -44,34 +53,60 @@ class Article:
         self.content_index.append("paragraph")
         self.content.append(paragraph)
 
-    # private method
-    def get_next_content(self):
-        if len(self.content_index) <= 0:
-            return "empty"
-        content_type = self.content_index.pop(0)
-        return (content_type, self.content.pop(0))
+    def fill_article_from_json_file(self, file):
+        data = json.load(file)
+
+        self.news_source = data['news_source']
+        self.date_and_time_updated = data['date_and_time_updated']
+        self.bookmarked = data['bookmarked']
+        self.opened = data['opened']
+
+        self.title_0 = data['title_0']
+        self.title_1 = data['title_1']
+        self.subtitle = data['subtitle']
+        self.author = data['author']
+        self.date_and_time = data['date_and_time']
+        self.content_index = data['content_index']
+        self.content = data['content']
+
+    def get_content(self):
+        return (content_index, content)
     
-    def get_json_article(self):
+    def get_json(self):
         data = {}
         data['news_source'] = self.news_source
         data['date_and_time_updated'] = self.date_and_time_updated
         data['bookmarked'] = self.bookmarked
         data['opened'] = self.opened
+
         data['title_0'] = self.title_0
         data['title_1'] = self.title_1
         data['subtitle'] = self.subtitle
         data['author'] = self.author
         data['date_and_time'] = self.date_and_time
-        data['content'] = []
-        data['content_index'] = []
-        while True:
-            content = self.get_next_content()
-            if content == "empty":
-                break
-            data['content_index'].append(content[0])
-            data['content'].append(content[1])
-
-        
+        data['content_index'] = self.content_index
+        data['content'] = self.content
         
         json_file = json.dumps(data, indent = 4)
         return json_file
+
+    def get_html(self):
+        html = "<qt>\n<body>\n"
+        html += self.get_tagged_string(HTMLTag.TITLE_0, self.title_0)
+        html += self.get_tagged_string(HTMLTag.TITLE_1, self.title_1)
+        html += self.get_tagged_string(HTMLTag.SUBTITLE, self.subtitle)
+        html += self.get_tagged_string(HTMLTag.AUTHOR, self.author)
+        html += self.get_tagged_string(HTMLTag.DATE_AND_TIME, self.date_and_time)
+
+        i = 0
+        for c in self.content_index:
+            if c == 'tagline':
+                html += self.get_tagged_string(HTMLTag.TAGLINE, self.content[i])
+            elif c == 'paragraph':
+                html += self.get_tagged_string(HTMLTag.PARAGRAPH, self.content[i])
+            i += 1
+        html += "<body>\n<qt>"
+        return html
+
+    def get_tagged_string(self, HTMLTag, str):
+        return '<' + HTMLTag + '>' + str + '</' + HTMLTag + '>\n'
--- logic/file_handler.py
+++ logic/file_handler.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import os
+import glob
 from pathlib import Path
 from article import Article, NewsSource
 
@@ -20,8 +21,20 @@ def safe_article(article):
     source = article.news_source
     title = article.title_0
     file = open(DIR_ARTICLES + '/' + source + ' - ' + title + '.json', 'w')
-    file.write(article.get_json_article())
+    file.write(article.get_json())
     file.close()
+
+def get_article_by_path(path):
+    article = Article(os.path.split(path)[1].split(' - ')[0]) # get News Source
+    file = open(path, 'r')
+    article.fill_article_from_json_file(file)
+    file.close()
+    return article
+
+
+
+def get_article_list():
+    return glob.glob(DIR_ARTICLES + '/*.json')
 
 #def read_article():
     
@@ -29,13 +42,20 @@ def safe_article(article):
 def __delete_articles():
     todo
 
-
-article_test = Article(NewsSource.DIE_ZEIT)
+store_files()
+article_test = Article(NewsSource.SRF)
+article_test.set_title_0("Article Title 0")
+article_test.set_title_1("Article Title 1")
+article_test.set_subtitle("Subtitle")
+article_test.set_author("Hans Franz Sebastian Barthalomeus")
+article_test.set_time_and_date("14:23, 18. April 2021")
 article_test.add_paragraph("PARAGRAPH")
 article_test.add_paragraph("PAR 2")
 article_test.add_tagline("taglin text")
 article_test.add_paragraph("Paragraph 3")
 article_test.add_tagline("tagline number 2")
-article_test.set_title_0("test title")
 safe_article(article_test)
-store_files()
+#print(article_test.get_html())
+#print(get_article_list())
+#a = get_article_by_path(get_article_list()[1])
+#print(a.get_html())
